a563e80dc8dc3d4e182f53fbb6590b982739a1c1
Rohan Ghosh <iamronnie40@gmail.com>
2020-05-11 11:23:05 +0530

    Add files via upload

---
+++ Lecture2.py
@@ -0,0 +1,17 @@
+r = 10
+area =3.14*r*r
+print(area)
+
+#Taking user input
+
+print("Print enter radius=")
+r = int(input())
+area =3.14*r*r
+print("The area is ")
+print(area)
+
+#Strings
+
+str1 = "Rohan"
+str2 = "Ghosh"
+print(str1+ " "+str2)
