e465bea2b1e280c5b04c4cfabc5e1e076802ac27
Rohan Ghosh <iamronnie40@gmail.com>
2020-05-15 20:48:24 +0530

    Add files via upload

---
+++ Lecture3.py
@@ -0,0 +1,16 @@
+#if else
+
+h = int(input())
+if(h>10):
+	print("The number is greater than 10")
+elif(h<10):
+	print("The number is less than 10")
+else:
+	print("Equal")
+
+#while loop
+
+while(h>10):
+	print("Hello")
+	h = h-1
+
