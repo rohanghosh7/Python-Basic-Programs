6ae1467138f926df4e89e89158267d5f144b63e3
Rohan Ghosh <iamronnie40@gmail.com>
2020-05-16 19:45:05 +0530

    Add files via upload

---
+++ Lecture4_1.py
@@ -0,0 +1,24 @@
+#for loop
+rohan = 10
+
+for x in range(2,30,2):
+	print(x)
+
+#functions
+
+def myfunc(a,b):
+	print(a+b)
+
+myfunc(10,7)      
+
+
+def myfunc2(a,b):
+	return a+b
+
+result = myfunc2(10,7)
+print(result)
+
+def printHello():
+	print("Hello")
+
+rohan = 10
